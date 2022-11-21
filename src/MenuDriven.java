c65ab3867e26feaf99586635ac46dbfba13909b6
Amirtha M Lalas <Amirtha@NTP-LAP-746.chn.nestgroup.net>
2022-11-21 13:14:28 +0530

    added reverse

--- src/MenuDriven.java
+++ src/MenuDriven.java
@@ -75,6 +75,20 @@ public class MenuDriven {
                         System.out.println(e+": is odd");
                     }
                     break;
+                case 5:
+                    int num;
+                    System.out.println("Enter the number");
+                    num = sc.nextInt();
+                    int rev=0;
+                    while(num!=0){
+
+                        int rem = num%10;
+                        rev = rev*10+rem;
+                        num=num/10;
+
+                    }
+                    System.out.println("reverse of number : "+rev);
+                    break;
             }
             System.out.println("Do you want to continue:1 for yes 2 for no");
 
